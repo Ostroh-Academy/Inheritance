d9b54aef3691b2e0f220e8547bf6e53c1770d0cb
Iryna Krush <145108812+Ira-name@users.noreply.github.com>
2024-03-28 13:58:21 +0200

    Add files via upload

---
+++ Function.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_P
+{
+	public class Function
+	{
+		private double a1, a0, b1, b0;
+		public Function(double a1, double a0, double b1, double b0)
+		{
+			this.a1 = a1;
+			this.a0 = a0;
+			this.b1 = b1;
+			this.b0 = b0;
+		}
+		public void Coefficients(double a1, double a0, double b1, double b0)
+		{
+			this.a1 = a1;
+			this.a0 = a0;
+			this.b1 = b1;
+			this.b0 = b0;
+		}
+		public void PrintCoefficients()
+		{
+			Console.WriteLine($"a1 = {a1}, a0 = {a0}, b1 = {b1}, b0 = {b0}");
+		}
+		public double Calculation(double x)
+		{
+			return (a1 * x + a0) / (b1 * x + b0);
+		}
+	}
+
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Lab_3_P;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Function fractionalLinearFun = new Function(2, 3, 4, 5);
+		Function2 fractionalFun = new Function2(1, 2, 3, 4, 5, 6);
+
+		Console.WriteLine("Fractional Linear Function coefficients:");
+		fractionalLinearFun.PrintCoefficients();
+		Console.WriteLine("Fractional Function coefficients:");
+		fractionalFun.PrintCoefficients();
+
+		double x = 2.5;
+		Console.WriteLine($"Value of Fractional Linear Function at x = {x}: {fractionalLinearFun.Calculation(x)}");
+		Console.WriteLine($"Value of Fractional Function at x = {x}: {fractionalFun.Calculation(x)}");
+	}
+}
