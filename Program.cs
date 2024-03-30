4113ddf7405b30fb39758a19fbd7d44f95330b13
Viktoriia Babaieva <145108530+Babakamaker@users.noreply.github.com>
2024-03-30 23:30:42 +0200

    lab_3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Triangles;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter side length of the equilateral triangle:");
+        double sideLengthEquilateral = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Equilateral Triangle Characteristics:");
+        EquilateralTriangle equilateralTriangle = new EquilateralTriangle();
+        equilateralTriangle.SetValues(sideLengthEquilateral, 60);
+
+        Console.WriteLine("Side Lengths: " + string.Join(", ", equilateralTriangle.GetSideLengths()));
+        Console.WriteLine("Perimeter: " + equilateralTriangle.CalculatePerimeter());
+        Console.WriteLine();
+
+        Console.WriteLine("Enter side length of the triangle:");
+        double sideLengthTriangle = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter first angle of the triangle:");
+        double firstAngleTriangle = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter second angle of the triangle:");
+        double secondAngleTriangle = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Triangle Characteristics:");
+        Triangle triangle = new Triangle();
+        triangle.SetValues(sideLengthTriangle, firstAngleTriangle, secondAngleTriangle);
+        Console.WriteLine("Side Lengths: " + string.Join(", ", triangle.GetSideLengths()));
+        Console.WriteLine("Angles: " + string.Join(", ", triangle.GetAngles()));
+        Console.WriteLine("Perimeter: " + triangle.CalculatePerimeter());
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,40 @@
+﻿namespace Triangles
+{
+    public class Triangle : EquilateralTriangle
+    {
+        public double secondAngle;
+
+        public new void SetValues(double sideLength, double firstAngle, double secondAngle)
+        {
+            base.SetValues(sideLength, firstAngle);
+            this.secondAngle = secondAngle;
+        }
+
+        public override double[] GetSideLengths()
+        {
+            double[] sideLengths = new double[3];
+            sideLengths[0] = sideLength;
+            sideLengths[1] = Math.Round((sideLength * Math.Sin(Math.PI * secondAngle / 180)) / Math.Sin(Math.PI * angle / 180));
+            sideLengths[2] = Math.Round((sideLength * Math.Sin(Math.PI * (180 - angle - secondAngle) / 180)) / Math.Sin(Math.PI * angle / 180));
+            return sideLengths;
+        }
+
+        public double[] GetAngles()
+        {
+             
+            return new double[] { angle, (180 - angle - secondAngle), secondAngle };
+        }
+
+        public override double CalculatePerimeter()
+        {
+
+            double[] sideLengths = GetSideLengths();
+            double perimeter = 0;
+            foreach (double side in sideLengths)
+            {
+                perimeter += side;
+            }
+            return perimeter;
+        }
+    }
+}
