6a6193a0fc4195a20cb8f798853694652c25ae3f
narberalAntares <valerii.deineka.volodymyrovych@gmail.com>
2024-03-31 23:53:13 +0300

    Finished Lab3

---
+++ 03-inheritance-keroNight/03-inheritance-keroNight/LinearFunction.cs
@@ -0,0 +1,25 @@
+﻿namespace _03_inheritance_keroNight;
+
+public class LinearFunction
+{
+    protected double[] coefficients;
+
+    public LinearFunction() => 
+        coefficients = new double[2]; // для лінійної функції y = a1x + a0
+
+    public void SetCoefficients(double a, double b)
+    {
+        coefficients[0] = a;
+        coefficients[1] = b;
+    }
+
+    public void DisplayCoefficients()
+    {
+        Console.WriteLine("Linear Function Coefficients:");
+        Console.WriteLine("Slope (m): " + coefficients[0]);
+        Console.WriteLine("Intercept (c): " + coefficients[1]);
+    }
+
+    public double CalculateValue(double x) => 
+        coefficients[0] * x + coefficients[1];
+}
---
+++ 03-inheritance-keroNight/03-inheritance-keroNight/Polynomial.cs
@@ -0,0 +1,28 @@
+﻿namespace _03_inheritance_keroNight;
+
+public class Polynomial : LinearFunction
+{
+    public Polynomial() => 
+        coefficients = new double[5]; // для многочлена a4x^4 + a3x^3 + a2x^2 + a1x + a0
+
+    public void SetCoefficients(double a)
+    {
+        for (var i = 0; i < 5; i++) 
+            coefficients[i] = a;
+    }
+
+    public new void DisplayCoefficients()
+    {
+        Console.WriteLine("Polynomial Coefficients:");
+        Console.WriteLine("a: " + coefficients[0]);
+    }
+
+    public new double CalculateValue(double x)
+    {
+        double result = 0;
+        for (var i = 0; i < 5; i++) 
+            result += coefficients[i] * Math.Pow(x, 4 - i);
+        
+        return result;
+    }
+}
