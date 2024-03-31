ecab0e76374ed970e76249c79e1578606768970b
Valera Sloza <valera.sloza@gmail.com>
2024-04-01 01:02:59 +0300

    Виконання

---
+++ Laba2/Laba2/Fraction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    public class Fraction
+    {
+        
+    protected double a;
+
+        public Fraction(double coefficient)
+        {
+            a = coefficient;
+        }
+
+        public void SetCoefficient(double coefficient)
+        {
+            a = coefficient;
+        }
+
+        public void PrintCoefficient()
+        {
+            Console.WriteLine("Coefficient a: " + a);
+        }
+
+        public double CalculateFractionValue(double x)
+        {
+            return 1 / (a * x);
+        }
+    }
+}
---
+++ Laba2/Laba2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Laba2;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fraction fraction = new Fraction(2);
+        fraction.PrintCoefficient();
+
+        Console.Write("Enter the value of x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fraction value at x = " + x + ": " + fraction.CalculateFractionValue(x));
+
+        ThreeDimensionalFraction threeDimensionalFraction = new ThreeDimensionalFraction(3, 4, 5);
+        threeDimensionalFraction.PrintCoefficient();
+
+        Console.Write("Enter the value of x: ");
+        x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Three-dimensional fraction value at x = " + x + ": " + threeDimensionalFraction.CalculateFractionValue(x));
+    }
+}
