173aa6a0d2a76691cc728c023f689ca42e8b6d0c
Анастасія Черній <jesikafan@gmail.com>
2024-03-31 19:50:36 +0300

    Лаб.№3 А.Черній ЕК-4

---
+++ L3 AChernii EC-4/Matrix2D.cs
@@ -0,0 +1,55 @@
+﻿namespace MatrixApp
+{
+    class Matrix2D
+    {
+        private int[,] matrix;
+
+        public Matrix2D(int rows, int cols)
+        {
+            matrix = new int[rows, cols];
+        }
+        public void Set()
+        {
+            Console.WriteLine("Введiть двовимiрної елементи матрицi:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"matrix[{i},{j}] = ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public void Random()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(-100, 101);
+                    Console.Write($"A[{i},{j}] = {matrix[i, j]}\n");
+                }
+            }
+        }
+        public int Min()
+        {
+            int minElement = matrix[0, 0];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < minElement)
+                    {
+                        minElement = matrix[i, j];
+                    }
+                }
+            }
+
+            return minElement;
+        }
+    }
+
+}
