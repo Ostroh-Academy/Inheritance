f76d1af02afd81973e48f63241bb5c62a3fe1e8b
nesterchuk.iraida03@gmail.com <nesterchuk.iraida03@gmail.com>
2024-04-09 20:08:01 +0300

    Виконала завдання лабораторної роботи 3 "Наслідування". Нестерчук Іраїда

---
+++ lab 3/lab 3/PointTransformation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    // Клас для перетворення точки
+    class PointTransformation
+    {
+        // Коефіцієнти перетворення
+        private double a11, a12, a13, a21, a22, a23, a31, a32, a33;
+
+        // Метод для задання коефіцієнтів перетворення з клавіатури
+        public void SetTransformationCoefficientsFromUserInput()
+        {
+            Console.WriteLine("Введіть коефіцієнти для трасформації точки:");
+            Console.Write("a11: ");
+            a11 = double.Parse(Console.ReadLine());
+            Console.Write("a12: ");
+            a12 = double.Parse(Console.ReadLine());
+            Console.Write("a13: ");
+            a13 = double.Parse(Console.ReadLine());
+            Console.Write("a21: ");
+            a21 = double.Parse(Console.ReadLine());
+            Console.Write("a22: ");
+            a22 = double.Parse(Console.ReadLine());
+            Console.Write("a23: ");
+            a23 = double.Parse(Console.ReadLine());
+            Console.Write("a31: ");
+            a31 = double.Parse(Console.ReadLine());
+            Console.Write("a32: ");
+            a32 = double.Parse(Console.ReadLine());
+            Console.Write("a33: ");
+            a33 = double.Parse(Console.ReadLine());
+        }
+
+        // Метод для виведення коефіцієнтів перетворення на екран
+        public void PrintTransformationCoefficients()
+        {
+            Console.WriteLine($"Коефіцієнти трансформації точки:");
+            Console.WriteLine($"a11 = {a11}, a12 = {a12}, a13 = {a13}");
+            Console.WriteLine($"a21 = {a21}, a22 = {a22}, a23 = {a23}");
+            Console.WriteLine($"a31 = {a31}, a32 = {a32}, a33 = {a33}");
+        }
+
+        // Метод для визначення образу заданої точки (x, y, z)
+        public void TransformPoint(double x, double y, double z)
+        {
+            double xNew = a11 * x + a12 * y + a13 * z;
+            double yNew = a21 * x + a22 * y + a23 * z;
+            double zNew = a31 * x + a32 * y + a33 * z;
+            Console.WriteLine($"Образ введеної користувачем точки: ({x}, {y}, {z}) = ({xNew}, {yNew}, {zNew})");
+        }
+    }
+}
---
+++ lab 3/lab 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Перетворення площини
+            PlaneTransformation planeTransform = new PlaneTransformation();
+            planeTransform.SetTransformationCoefficientsFromUserInput();
+            planeTransform.PrintTransformationCoefficients();
+
+            // Перетворення точки
+            PointTransformation pointTransform = new PointTransformation();
+            pointTransform.SetTransformationCoefficientsFromUserInput();
+            pointTransform.PrintTransformationCoefficients();
+
+            Console.WriteLine("Введіть початкові координати (x, y, z) для трансформації:");
+            Console.Write("x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            pointTransform.TransformPoint(x, y, z);
+
+            // Виклик методу для визначення образу точки на площині
+            planeTransform.TransformPointOnPlane(x, y);
+
+            Console.WriteLine("Натисніть будь-яку кнопку для виходу");
+            Console.ReadKey();
+        }
+    }
+}
